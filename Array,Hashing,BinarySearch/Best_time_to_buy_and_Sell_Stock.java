73969f68ade2785746c5c3fc9c0042c1e0e820e6
Aakash Sharma <94851495+aakashsharma7@users.noreply.github.com>
2024-02-21 11:15:26 +0530

    Create Best_time_to_buy_and_Sell_Stock.java

---
+++ Array,Hashing,BinarySearch/Best_time_to_buy_and_Sell_Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0)return 0;
+        int profit = 0;
+        int min = prices[0];
+        int max = 0;
+
+        for(int stockprices:prices){
+            min = Math.min(min,stockprices);
+            profit = stockprices-min;
+            max = Math.max(profit,max);
+        }
+        return max;
+    }
+}
