98c2ad5a78734dea3ec1fe3972c4428d71c94869
aakashsharma7 <aakashsharma.ghd@gmail.com>
2024-05-07 20:59:49 +0530

    java

---
+++ longest_repeating_character_replacement.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.*;
+public class longest_repeating_character_replacement {
+    public int characterReplacement(String s, int k) {
+        int lo = 0;
+        int[] arr = new int[26];
+        int max_count = 0;
+        int max_length = 0;
+
+        for(int hi = 0; hi<s.length(); hi++){
+            arr[s.charAt(hi)-'A']++;
+            int freq = arr[s.charAt(hi)-'A'];
+            max_count = Math.max(max_count,freq);
+            
+            while(hi- lo + 1 - max_count > k){
+                arr[s.charAt(lo)-'A']--;
+                lo++;
+            }
+            max_length = Math.max(max_length,hi-lo+1);
+        }
+        return max_length;
+    }
+}
