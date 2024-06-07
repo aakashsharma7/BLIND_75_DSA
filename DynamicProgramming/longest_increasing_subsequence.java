2631b3a1a58fdabf9d963273bd9b7a91528d0b0a
Aakash Sharma <94851495+aakashsharma7@users.noreply.github.com>
2024-06-07 22:52:56 +0530

    Create longest_increasing_subsequence.java

---
+++ DynamicProgramming/longest_increasing_subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int maxLength = 1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1; // Every element is a subsequence of length 1
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
